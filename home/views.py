86d694f87d6cc2102cdb7ef0427055fb61b10151
Aqib Bala <73388336+aqibmehrajbala@users.noreply.github.com>
2022-12-11 20:03:16 +0530

    feat (validation): some changes on validation

--- home/views.py
+++ home/views.py
@@ -12,7 +12,11 @@ def index(request):
 def create(request):
     if request.method == 'POST':
         if request.POST.get('text') == "" or request.POST.get('start') == "" or request.POST.get('end') == "":
-            error = "Field`s can`t be empty!"
+            error = "Field`s can not be empty!"
+            success = {"error": error}
+            return JsonResponse(success)
+        elif int(request.POST.get('start')) < 1 or int(request.POST.get('end')) < 1:
+            error = "Field must be greater than 0"
             success = {"error": error}
             return JsonResponse(success)
         elif int(request.POST.get('start')) > int(request.POST.get('end')):
@@ -20,14 +24,14 @@ def create(request):
             success = {"error": error}
             return JsonResponse(success)
         elif int(request.POST.get('end')) > len(request.POST.get('text')):
-            textLength = len(request.POST.get('text'));
+            textLength = len(request.POST.get('text'))
             error = f"The END field may not be longer than {textLength} characters"
             success = {"error": error}
             return JsonResponse(success)
         else:
-            text = request.POST['text']
-            start = request.POST['start']
-            end = request.POST['end']
+            text = str(request.POST['text'])
+            start = int(request.POST['start'])
+            end = int(request.POST['end'])
             new_assessment = assessment(text=text, start=start, end=end)
             new_assessment.save()
             success_message = "Data saved successfully!"
